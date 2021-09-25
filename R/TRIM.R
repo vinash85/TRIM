df196746aa0250b7fcc69e21f1956c5b7be2bd79
phillipnicol <43178367+phillipnicol@users.noreply.github.com>
2021-09-25 10:23:41 -0400

    Created package skeleton

--- R/TRIM.R
+++ R/TRIM.R
@@ -1,15 +1,13 @@
-TRIM = function(pathways, cistrome.hdf5.path, single.cell.reference = NULL) {
-  require(data.table)
-  require(tidyverse)
+
+#' @export 
+TRIM <- function(pathways, cistrome.hdf5.path, single.cell.reference = NULL) {
 	# regulatory (metabolic) module
-	metabolic.result = regulatory.pipeline(pathways.list, cistrome.location=cistrome.hdf5.path)
+	metabolic.result <- regulatory.pipeline(pathways.list, cistrome.location=cistrome.hdf5.path)
 	# immune module 
-	immune.result = immune.module()
+	immune.result <- immune.module()
 	# integration 
-	result = integration(metabolic.result, immune.result)
+	result <- integration(metabolic.result, immune.result)
 
 	list(result, metabolic.result=metabolic.result, immune.module=immune.result)
 
 }
-
-## Create plots (used in the main figure) using TRIM results
