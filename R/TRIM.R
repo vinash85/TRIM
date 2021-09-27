4792b71325185e908ddf551e5b321d8723335f24
Avinash <asahu@node23.cm.cluster>
2021-09-26 23:03:37 -0400

    today

--- R/TRIM.R
+++ R/TRIM.R
@@ -1,6 +1,6 @@
 
 #' @export 
-TRIM <- function(pathways, cistrome.hdf5.path, single.cell.reference = NULL) {
+TRIM <- function(pathways.list, cistrome.hdf5.path, single.cell.reference = NULL) {
 	# regulatory (metabolic) module
 	metabolic.result <- regulatory.pipeline(pathways.list, cistrome.location=cistrome.hdf5.path)
 	# immune module 
